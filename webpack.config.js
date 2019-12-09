8535d65d0508fed587bee6106c3abcc355d07c69
Zhiyuan He <zh2312@columbia.edu>
2019-12-09 17:10:34 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/photo32em.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  }
+};
