c43401d1e46b05a09196c2d2fe5c6af1d73b6ed7
Zhiyuan He <zh2312@columbia.edu>
2019-12-11 21:37:40 -0500

    fixed a few issues in #4

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import { AuthRoute } from "../util/route_util";
 
 import AuthHeader from "./auth_header/auth_header_container";
@@ -9,13 +9,23 @@ import SignupFormContainer from './session_forms/signup_form_container';
 const App = () => (
     <div>
       <header className="header">
-        <h1>32em</h1>
+        <Link to="/"><h1>32em</h1></Link>
         <AuthHeader/>
       </header>
       <main className="body">
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
       </main>
+      <footer className="footer">
+      <div className="cr-logo">&copy; 32em</div>
+      <div className="footer-links">
+        <ul>
+          <li><Link to="/">Terms</Link></li>
+          <li><Link to="/">Privacy</Link></li>
+          <li><Link to="/">Support</Link></li>
+        </ul>
+      </div>
+      </footer>
     </div>
   );
   
